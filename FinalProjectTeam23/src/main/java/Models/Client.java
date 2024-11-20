f5b1ef86eb0de3f3fa6bafb08879a89891ce05e8
Ariel Lockett <122488824+ariellockett2@users.noreply.github.com>
2024-11-20 09:52:30 -0800

    Added client class

--- FinalProjectTeam23/src/main/java/Models/Client.java
+++ FinalProjectTeam23/src/main/java/Models/Client.java
@@ -1,3 +1,39 @@
-public class Client {
-    
+import java.util.UUID;
+
+public class Client extends User {
+    private UUID clientID;
+    private String goals;
+    private String workoutPlan;
+
+    public Client(UUID userID, String username, String passwordHash, String email, String role, UUID clientID, String goals, String workoutPlan) {
+        super(userID, username, passwordHash, email, role);
+        this.clientID = clientID;
+        this.goals = goals;
+        this.workoutPlan = workoutPlan;
+    }
+
+    public UUID getClientID() {
+        return clientID;
+    }
+
+    public void setClientID(UUID clientID) {
+        this.clientID = clientID;
+    }
+
+    public String getGoals() {
+        return goals;
+    }
+
+    public void setGoals(String goals) {
+        this.goals = goals;
+    }
+
+    public String getWorkoutPlan() {
+        return workoutPlan;
+    }
+
+    public void setWorkoutPlan(String workoutPlan) {
+        this.workoutPlan = workoutPlan;
+    }
 }
+
